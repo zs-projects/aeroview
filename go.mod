e4bf931513e782d8edf6e66b77829c968c874c25
jeremie <jeremie.a.simon@gmail.com>
2020-06-16 14:49:31 +0200

    move to farmhash (#5)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module zs-project.org/aeroview
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13
+	github.com/stretchr/testify v1.6.1
+)
---
+++ mph/farmhash/farmhash.go
@@ -0,0 +1,22 @@
+package farmhash
+
+import (
+	"reflect"
+	"unsafe"
+
+	farm "github.com/dgryski/go-farm"
+)
+
+// Hash32 returns 32 bits hash of a string.
+func Hash32(s string) uint32 {
+	// Using unsafe here to avoid escaping to heap / copying bytes on heap.
+	// similar to reinterpret_cast in c++
+	var b []byte
+	hdr := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+	b = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: hdr.Data,
+		Len:  hdr.Len,
+		Cap:  hdr.Len,
+	}))
+	return farm.Fingerprint32(b)
+}
