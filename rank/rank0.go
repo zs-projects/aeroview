41124aedd45263d8f384fe97ff5d715b3279dcba
Jeremie Simon <jeremie.a.simon@gmail.com>
2020-07-20 19:36:04 +0200

    rank0

---
+++ rank/rank0.go
@@ -0,0 +1,77 @@
+package rank
+
+const (
+	smallBlockSize  = 8
+	bigBlockSize    = 64
+	smallToBigRatio = 8
+)
+
+type Rank0 struct {
+	bits       *BitVec
+	data       []uint64
+	blocks     []int
+	lookup     [256]uint8
+}
+
+func (r *Rank0) Get(idx int) uint64 {
+	if r.bits.Get(idx) == 0 {
+		return 0
+	}
+	var sum int
+	bigBlockIndex := idx / bigBlockSize
+	blockOffset := idx % bigBlockSize
+
+
+	// 1. get big block
+	if bigBlockIndex > 0 {
+		sum += r.blocks[bigBlockIndex - 1]
+	}
+
+	// 2. get sum from lookup
+	sum += r.rank(idx - blockOffset, idx)
+
+	return r.data[sum - 1]
+}
+
+func (r *Rank0) rank(low, idx int) int {
+	runningRank := 0
+	for i := low; i <= idx; {
+		var n uint8
+		if i+7 <= idx {
+			n = r.bits.Get8BitRange(i, i+7)
+		} else {
+			n = r.bits.Get8BitRange(i, idx)
+		}
+		runningRank += int(r.lookup[n])
+		i += 8
+	}
+	return runningRank
+}
+
+func NewRank0(xs []uint64) *Rank0 {
+	lookup := Make8BitLookup()
+
+	nBigBlocks := len(xs) / 64
+	blocks := make([]int, nBigBlocks + 1)
+	bits := NewBitVec(nBigBlocks)
+
+	var data []uint64
+	var s int
+	for i, x := range xs {
+
+		if x != 0 {
+			data = append(data, x)
+			s++
+			blocks[i / bigBlockSize] = s
+			bits.Set(i, 1)
+		}
+	}
+
+	return &Rank0{
+		bits:       bits,
+		data:       data,
+		blocks:     blocks,
+		lookup:     lookup,
+	}
+}
+
