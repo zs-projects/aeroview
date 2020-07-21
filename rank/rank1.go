fff7de0649ec9e022fb6b14903240cb5de8575f2
jeremie <jeremie.a.simon@gmail.com>
2020-07-21 16:09:18 +0200

    more test + rank1 (#14)

---
+++ rank/rank1.go
@@ -0,0 +1,85 @@
+package rank
+
+type Rank1 struct {
+	bits        *BitVec
+	data        []uint64
+	bigBlocks   []int
+	smallBlocks []uint8
+	lookup      [256]uint8
+}
+
+func (r *Rank1) Get(idx int) uint64 {
+	if r.bits.Get(idx) == 0 {
+		return 0
+	}
+	var sum int
+
+	// 1. get big block
+	bigBlockIndex := idx / bigBlockSize
+	if bigBlockIndex > 0 {
+		sum += r.bigBlocks[bigBlockIndex - 1]
+	}
+
+	// 2. get small block sum
+	smallBlockIndex := idx / smallBlockSize
+	smallBlockOffset := idx % smallBlockSize
+	sum += int(r.smallBlocks[smallBlockIndex])
+
+	// 3. get sum from lookup
+	lookupIndex := r.bits.Get8BitRange(idx - smallBlockOffset, idx)
+	sum += int(r.lookup[lookupIndex])
+
+	return r.data[sum - 1]
+}
+
+func NewRank1(xs []uint64) *Rank1 {
+	lookup := Make8BitLookup()
+
+	nBigBlocks := len(xs) / bigBlockSize
+	bigBlocks := make([]int, nBigBlocks+1)
+
+	nSmallBlocks := len(xs) / smallBlockSize
+	// +2 because front and back padding
+	smallBlocks := make([]uint8, nSmallBlocks + 2)
+
+	bits := NewBitVec(nBigBlocks)
+
+	var data []uint64
+	var bigBlockSum int
+	var smallBlockSum uint8
+
+	for i, x := range xs {
+
+		// keep running sum only within a big block
+		if i % bigBlockSize == 0 {
+			smallBlockSum = 0
+		}
+
+		if x != 0 {
+			// 1. data: keep non zeroed data
+			data = append(data, x)
+			bits.Set(i, 1)
+
+			// 2. big block sum
+			blockIndex := i/bigBlockSize
+			bigBlockSum++
+			bigBlocks[blockIndex] = bigBlockSum
+
+			// 3. small block
+			smallBlockSum++
+		}
+
+		smallBlockIndex := i/smallBlockSize
+		if smallBlockIndex % 8 != 7 {
+			smallBlocks[smallBlockIndex + 1] = smallBlockSum
+		}
+	}
+
+	return &Rank1{
+		bits:        bits,
+		data:        data,
+		bigBlocks:   bigBlocks,
+		smallBlocks: smallBlocks,
+		lookup:      lookup,
+	}
+}
--- rank/rank1_test.go
+++ rank/rank1_test.go
@@ -6,38 +6,39 @@ import (
 	"testing"
 )
 
-func TestB(t *testing.T) {
+func TestNewRank1(t *testing.T) {
 	xs := make([]uint64, 1000)
 	for i := 0; i < 1000; i++ {
 		if i % 2 == 0 {
 			xs[i] = uint64(i)
 		}
 	}
-	r0 := NewRank0(xs)
+	r1 := NewRank1(xs)
 	for i := 0; i < 1000; i++ {
-		assert.Equal(t, xs[i], r0.Get(i))
+		assert.Equal(t, xs[i], r1.Get(i), "%d %d", xs[i], r1.Get(i))
 	}
 }
 
-func TestRank0_Get(t *testing.T) {
+func TestRank1_Get(t *testing.T) {
 	collectionSize := 2000
 	nTries := 100
 	rand.Seed(1)
 
+	inc := uint64(0)
 	for i := 0; i < nTries; i++ {
 
 		// 1. build data
 		xs := make([]uint64, collectionSize)
 		for i := range xs {
 			if rand.Intn(100) < 20 {
-				xs[i] = rand.Uint64()
+				xs[i] = inc
+				inc++
 			}
 		}
 
-		b := NewRank0(xs)
+		b := NewRank1(xs)
 		for i := 0; i < len(xs); i++ {
-			assert.Equal(t, xs[i], b.Get(i))
+			assert.Equal(t, xs[i], b.Get(i), "%dth example, expected %d but was %d", i, xs[i], b.Get(i))
 		}
 	}
 }
-
