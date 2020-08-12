e48e79d660ec4437462b6c9964ec263d129a4c84
Ryad Zenine <r.zenine@gmail.com>
2020-08-13 00:46:13 +0200

    Add's unit test for radix tree

--- datastructures/radix_tree_test.go
+++ datastructures/radix_tree_test.go
@@ -8,15 +8,44 @@ import (
 
 func TestMakeRadixTree(t *testing.T) {
 	data := []string{"string1", "string2", "dance", "opera", "ope2", "winter", "alstom", "netscape", "hi", "lower", "high"}
+	root := []string{"string", "dance", "ope", "winter", "alstom", "netscape", "hi", "lower"}
+	expectedStructure := RadixTree{
+		children: map[string]*RadixTree{
+			"string": {
+				children: map[string]*RadixTree{
+					"1": {isLeaf: true},
+					"2": {isLeaf: true},
+				},
+			},
+			"ope": {
+				children: map[string]*RadixTree{
+					"ra": {isLeaf: true},
+					"2":  {isLeaf: true},
+				},
+			},
+			"hi": {
+				children: map[string]*RadixTree{
+					"gh": {isLeaf: true},
+				},
+				isLeaf: true,
+			},
+			"alstom":   {isLeaf: true},
+			"lower":    {isLeaf: true},
+			"netscape": {isLeaf: true},
+			"winter":   {isLeaf: true},
+			"dance":    {isLeaf: true},
+		},
+	}
 	rdt := MakeRadixTree(data)
-	if _, ok := rdt.children["string"]; !ok {
-		t.Errorf("Expected the root node to contain 'string'")
+	for _, v := range root {
+		if _, ok := rdt.children[v]; !ok {
+			t.Errorf("Expected to find %v as prefix", v)
+		}
 	}
-
-	if _, ok := rdt.children["hi"]; !ok {
-		t.Errorf("Expected the root node to contain 'h'")
+	diff := pretty.Diff(rdt, expectedStructure)
+	if len(diff) != 0 || diff != nil {
+		t.Errorf("Radix Tree not as expected %# v", diff)
 	}
-	t.Errorf("%# v", pretty.Formatter(rdt))
 }
 
 func TestCompressSubTrie(t *testing.T) {
