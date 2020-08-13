51a466b85b70e0155d7439e6039f8a54af03658c
Ryad Zenine <r.zenine@gmail.com>
2020-08-14 01:22:32 +0200

    Cleanup bitqueue implementation

---
+++ datastructures/bits/queue.go
@@ -0,0 +1,118 @@
+package bits
+
+import (
+	"fmt"
+)
+
+// Queue implements a FIFO queue on top of BitVec.
+type Queue struct {
+	data BitVec
+	// cursor represent the position of the next free bit.
+	cursor int
+	// remainingCapacity is the number of unused bits in the current word.
+	remainingCapacity int8
+}
+
+// MakeQueue creates a BitQueue with 0 capacity
+func MakeQueue() Queue {
+	return Queue{
+		data:              make([]uint64, 0),
+		remainingCapacity: 0,
+		cursor:            0,
+	}
+}
+
+// MakeBitQueueFromSlice makes a BitQueue from a slice.
+func MakeBitQueueFromSlice(b []uint64, size int) (Queue, error) {
+	if len(b)*BLOCKSIZE-size > BLOCKSIZE-1 {
+		return Queue{}, fmt.Errorf("size is invalid, it should be between %v and %v", len(b)*8, len(b)*8-7)
+	}
+	return Queue{
+		data:              b,
+		remainingCapacity: int8(len(b)*BLOCKSIZE - size),
+		cursor:            0,
+	}, nil
+}
+
+// Len returns the number of bits stored.
+func (m Queue) Len() int {
+	return len(m.data)*BLOCKSIZE - int(m.remainingCapacity)
+}
+
+// PushBack add the provided bit at the end of the BitQueue
+// bit should be 0 or 1 ( otherwise the function will take the lowest bit value anyway )
+// Assumes little endian encoding.
+func (m *Queue) PushBack(bit uint64) {
+	if m.remainingCapacity > 0 {
+		newValue := m.data[len(m.data)-1] | uint64((bit&0b1)<<(BLOCKSIZE-m.remainingCapacity))
+		m.data[len(m.data)-1] = newValue
+		m.remainingCapacity--
+	} else {
+		m.data = append(m.data, uint64((bit & 0b1)))
+		m.remainingCapacity = (BLOCKSIZE - 1)
+	}
+}
+
+// Pop pops one bit from the queue
+func (m *Queue) Pop() uint64 {
+	v := m.Peek()
+	m.cursor++
+	return v
+}
+
+// Peek return the next from the queue withouth removing it.
+func (m Queue) Peek() uint64 {
+	return m.Get(m.cursor)
+}
+
+// Empty return true if the queue is empty
+func (m Queue) Empty() bool {
+	return m.cursor >= m.Len()
+}
+
+// Data returns the a copy of the underlying data as []byte.
+func (m Queue) Data() []uint64 {
+	b := make([]uint64, len(m.data))
+	copy(b, m.data)
+	return b
+}
+
+// Reset resets the queue.
+func (m *Queue) Reset() {
+	m.cursor = 0
+}
+
+// Append and the bytes in the slice to the buffer.
+func (m *Queue) Append(data []uint64, size int) {
+	b, _ := MakeBitQueueFromSlice(data, size)
+	for !b.Empty() {
+		m.PushBack(b.Pop())
+	}
+}
+
+// Get return the bit value at position int
+func (m Queue) Get(i int) uint64 {
+	position := i / BLOCKSIZE
+	offset := i % BLOCKSIZE
+	return m.data[position] >> offset & 0b1
+}
+
+// Toggle set the bit balue at position i
+// does nothing if i is out of bound
+func (m Queue) Toggle(i int) {
+	position := i / BLOCKSIZE
+	if position < len(m.data) {
+		offset := i % BLOCKSIZE
+		m.data[position] = m.data[position] ^ 1<<(offset)
+	}
+}
+
+// High set the bit balue at position i
+// does nothing if i is out of bound
+func (m Queue) High(i int) {
+	position := i / BLOCKSIZE
+	if position < len(m.data) {
+		offset := i % BLOCKSIZE
+		m.data[position] = m.data[position] | 1<<offset
+	}
+}
---
+++ datastructures/bits/queue_test.go
@@ -0,0 +1,96 @@
+package bits
+
+import (
+	"testing"
+)
+
+func TestBitQueuePushBack(t *testing.T) {
+	// Tests that push back is working as expected.
+	b := MakeQueue()
+	b.PushBack(1)
+	vals := b.Data()
+	if len(vals) != 1 {
+		t.Errorf("Expected the length of the bitQueue to be one. got %v", b.Len())
+	}
+	if vals[0] != 1 {
+		t.Errorf("Expected the first value of the bitQueue to be one. got %v", vals[0])
+	}
+	b.PushBack(0)
+	if vals[0] != 1 {
+		t.Errorf("Expected the first value of the bitQueue to be 10 got %#b", vals[0])
+	}
+	b.PushBack(0)
+	b.PushBack(1)
+	if b.data[0] != 9 {
+		t.Errorf("Expected the first value of the bitQueue to be %b ans was %#b", 9, b.data[0])
+	}
+	b.PushBack(0)
+	b.PushBack(0)
+	b.PushBack(1)
+	b.PushBack(0)
+	if b.data[0] != 73 {
+		t.Errorf("Expected the first value of the bitQueue to be 0b10010010 ans was %#b", b.data[0])
+	}
+	for i := 0; i < 56; i++ {
+		b.PushBack(0)
+	}
+	b.PushBack(1)
+	if len(b.Data()) != 2 {
+		t.Errorf("Expected the length of the bitQueue to be two. got %v", b.Len())
+	}
+	if b.Data()[1] != 1 {
+		t.Errorf("Expected the second value of the bitQueue to be one. got %#b", b.Data()[1])
+	}
+	// Tests that Pushback is always only using the least significat bit.
+	b = MakeQueue()
+	b.PushBack(2)
+	vals = b.Data()
+	if len(vals) != 1 {
+		t.Errorf("Expected the length of the bitQueue to be one. got %v", b.Len())
+	}
+	if vals[0] != 0 {
+		t.Errorf("Expected the first value of the bitQueue to be 0. got %v", vals[0])
+	}
+}
+
+func TestBitQueueAppend(t *testing.T) {
+	b := MakeQueue()
+	b.PushBack(1)
+	b.PushBack(0)
+	b.PushBack(1)
+
+	b.Append([]uint64{uint64(0b10110100)}, 6)
+
+	if b.data[0] != uint64(0b00110100101) {
+		t.Errorf("BitQueue Append Failed.")
+	}
+}
+
+func TestBitQueueToggle(t *testing.T) {
+	b := MakeQueue()
+	b.PushBack(1)
+	b.PushBack(0)
+	b.PushBack(1)
+
+	b.Append([]uint64{uint64(0b10110100)}, 6)
+	b.Toggle(0)
+	b.Toggle(3)
+	b.Toggle(4)
+	if b.data[0] != uint64(0b00110111100) {
+		t.Errorf("BitQueue Set Failed.")
+	}
+}
+
+func TestBitQueueHigh(t *testing.T) {
+	b := MakeQueue()
+	b.PushBack(1)
+	b.PushBack(0)
+	b.PushBack(1)
+
+	b.Append([]uint64{uint64(0b10110100)}, 6)
+	b.High(6)
+	b.High(0)
+	if b.data[0] != uint64(0b00111100101) {
+		t.Errorf("BitQueue Set Failed.")
+	}
+}
