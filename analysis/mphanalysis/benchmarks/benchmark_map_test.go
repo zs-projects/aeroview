db43e27a261ef738db9e18d50ddf5c77bf057fc9
Ryad Zenine <r.zenine@gmail.com>
2020-08-20 21:36:54 +0200

    Benchmark Chd0

--- analysis/mphanalysis/benchmarks/benchmark_map_test.go
+++ analysis/mphanalysis/benchmarks/benchmark_map_test.go
@@ -3,6 +3,7 @@ package benchmarks
 import (
 	"testing"
 
+	"github.com/zs-projects/aeroview/mph/chd0"
 	"github.com/zs-projects/aeroview/mph/recsplit"
 )
 
@@ -27,6 +28,34 @@ func BenchmarkRecsplitGet100K(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, key := range keys {
 			dummy = mph.Get(key)
+		}
+	}
+	b.ReportMetric(float64(1000), "Get/op")
+}
+
+func BenchmarkRecsplitSimpleGet100K(b *testing.B) {
+	ds, keys := makeDataset(100000, "string", 1000)
+
+	mph := recsplit.Recsplit(recsplit.FromMap(ds, 20))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, key := range keys {
+			dummy = mph.Get(key)
+		}
+	}
+	b.ReportMetric(float64(1000), "Get/op")
+}
+func BenchmarkCHDGet100K(b *testing.B) {
+	ds, keys := makeDataset(100000, "string", 1000)
+
+	mph, err := chd0.FromMap(ds)
+	if err != nil {
+		panic("CHD failed")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, key := range keys {
+			dummy, _ = mph.Get(key)
 		}
 	}
 	b.ReportMetric(float64(1000), "Get/op")
